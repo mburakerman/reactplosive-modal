35173c4cb308aa31a9ca1d4090b439a2f4f85606
mburakerman <mburakerman@gmail.com>
2022-11-25 17:52:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reactplosive-modal
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-zp5tke)
---
+++ src/components/ReactplosiveModal/index.tsx
@@ -0,0 +1,83 @@
+import { ReactNode, useEffect, useState } from 'react';
+import './style.scss';
+import explosionGif from './explosion-gif';
+
+type Props = {
+  title?: ReactNode;
+  children: ReactNode;
+  isVisible: boolean;
+  onClose: (visibility: boolean) => void;
+  outsideClickEnabled?: boolean;
+};
+
+const ReactplosiveModal = ({
+  title,
+  children,
+  isVisible,
+  onClose,
+  outsideClickEnabled = true,
+}: Props) => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setActive(isVisible), 400);
+    return () => clearTimeout(timeout);
+  }, [isVisible]);
+
+  return (
+    <div
+      className={
+        isVisible
+          ? 'reactplosive-modal reactplosive-modal--active'
+          : 'reactplosive-modal'
+      }
+    >
+      <div
+        className={
+          active
+            ? 'reactplosive-modal-container reactplosive-modal-container---active'
+            : 'reactplosive-modal-container'
+        }
+      >
+        <button
+          className="reactplosive-modal-close"
+          onClick={() => onClose(false)}
+        >
+          <CloseIcon />
+        </button>
+        {title ? (
+          <div className="reactplosive-modal-header">
+            <div className="reactplosive-modal-header-title">{title}</div>
+          </div>
+        ) : null}
+
+        <div className={'reactplosive-modal-content'}>{children}</div>
+      </div>
+      <div
+        className="reactplosive-modal-bg"
+        onClick={() => (outsideClickEnabled ? onClose(false) : null)}
+      >
+        <img
+          className="reactplosive-modal-explosion-gif"
+          src={isVisible ? explosionGif : ''}
+        />
+      </div>
+    </div>
+  );
+};
+
+const CloseIcon = ({ color = 'rgba(0, 0, 0, 0.2)' }: { color?: string }) => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line x1="1" y1="11" x2="11" y2="1" stroke={color} stroke-width="2" />
+      <line x1="1" y1="1" x2="11" y2="11" stroke={color} stroke-width="2" />
+    </svg>
+  );
+};
+
+export default ReactplosiveModal;
